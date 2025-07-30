6ee75e17f59bef4e4f49d8204f52dfafe4f262c9
Firebase Studio <noreply@firebase.studio>
2025-07-30 00:46:06 +0000

    Initialized workspace with Firebase Studio

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyCzq1OkVpN-hwhDcW5ClwDqT9xWrceSXy4",
+
+    firebase: {
+      apiKey: "AIzaSyCRQ0Ob36WvCBn8uzDQtLEyEvWmdFgVxr8",
+      authDomain: "docsafer-ai-app.firebaseapp.com",
+      projectId: "docsafer-ai-app",
+      storageBucket: "docsafer-ai-app.firebasestorage.app",
+      messagingSenderId: "1037504782121",
+      appId: "1:1037504782121:web:68a350bf132d20ac01e138",
+    }
+    };
